f712f116a61a82aadde887147fdaccdd12e289e7
Jochnickel <47549862+Jochnickel@users.noreply.github.com>
2020-01-07 13:58:37 +0100

    Update addon_game_mode.lua

--- game/dota_addons/addon_template_debug/scripts/vscripts/addon_game_mode.lua
+++ game/dota_addons/addon_template_debug/scripts/vscripts/addon_game_mode.lua
@@ -1,6 +1,13 @@
 local ADDON_FOLDER = debug.getinfo(1,"S").source:sub(2,-37)
 
 
+local GROUP_INFO = {
+	Global = "Global functions. These can be called without any class.",
+	CBaseEntity = "The base class for stuff",
+	CEntities = "''Global accessor variable:'' <code>Entities</code>",
+}
+
+
 function forOrdered(t,hFunc)
 	if "table"~=type(t) then return end
 	local keys = {}
@@ -16,76 +23,45 @@ end
 
 
 function Activate()
-	dumpGlobals()
+	DumpScriptBindings()
 	wikiCreator()
 end
 
-
-
-function dumpGlobals()
-	local seenTables = {}
-	local file1
-	local file2
-	function printDump(path,t)
-		-- print(path,type(t),t)
-		file1:write(path.." "..type(t).." "..tostring(t).."\n")
-		if "userdata"==type(t) then
-			-- local a = string.gsub(tostring(t),"\n","YOLO")
-			file2:write(path.." "..type(t).." "..tostring(t).."\n")
-		end
-	end
-	
-
-	function dump(t,path)
-		if seenTables[t] then 
-			printDump(path.." == "..seenTables[t],t)
-			return
-		else
-			printDump(path,t)
-		end
-		if "table"~=type(t) then return end
-		seenTables[t] = path
-		local keys = {}
-		for k in pairs(t) do
-			table.insert(keys,k)
-		end
-		table.sort(keys)
-		for _,k in ipairs(keys) do
-			local v = t[k]
-			if "string"==type(k) then
-				dump(v,path.."."..k)
-			else
-				dump(v,path.."["..k.."]")
-			end
-		end
-	end
-	file1 = io.open(ADDON_FOLDER.."dump1.txt","w")
-	file2 = io.open(ADDON_FOLDER.."dump2.txt","w")
-	dump(_G,"_G")
-	file1:close()
-	file2:close()
-end
-
-
 function wikiCreator()
+	local preWiki = "{{otherlang2\n|zh-cn=Dota 2 Workshop Tools/Scripting/API:zh-cn\n|ru=Dota 2 Workshop Tools/Scripting/API:ru\n}}{{Note | This page is automatically generated. Changes can be submitted on Jochnickels github page: https://github.com/Jochnickel/dota_wiki_dumper}}\n  \n==='''Accessing the DOTA 2 Scripting API from Lua===\n\nWhile Lua is [http://en.wikipedia.org/wiki/Dynamically_typed dynamically typed], the DOTA 2 engine is written primarily in C++, which is [http://en.wikipedia.org/wiki/Type_system#Static_type-checking statically typed]. Thus, you'll need to be conscious of your data types when calling the API. (If you try to pass the wrong type to an API function, you'll get an error message in Vconsole telling you what you passed and what it was expecting.)\n__TOC__"
 	local file = io.open(ADDON_FOLDER.."dumpWiki.txt","w")
-
-	function subPage(tab,preText,groupname)
-		file:write(preText)
-		forOrdered(tab,function(funcName,desc)
-			local descParsed = string.gsub(tostring(desc),"\n","</code>\n| ")
-			file:write("|-\n| [[Dota 2 Workshop Tools/Scripting/API/"..groupname.."."..funcName.." | "..funcName.."]]\n")
-			file:write("| <code>"..descParsed.."\n")
-		end)
-		file:write("|}\n")
-	end
-	
-	subPage(
-		_G.FDesc,
-		"=== Global ===\n''Global functions.  These can be called without any class''\n{| class=\"standard-table\" style=\"width: 100%;\"\n! Function \n! Signature \n! Description \n",
-		"Global"
-	)
-
+	file:write(preWiki)
+	file:write(generateGroup("Global",_G.FDesc))
+	forOrdered(_G.CDesc,function(className,class)
+		file:write(generateGroup(className,class.FDesc))
+	end)
 	file:close()
 	
+end
+
+function generateGroup(groupName,desc) -- returns wiki string
+	local extendedClass = getmetatable( desc ) and getmetatable( desc ).__index
+	local extendedClassName = "" for k,v in pairs(_G.CDesc) do if extendedClass==v then extendedClassName=k end end
+	local out = string.format("=== %s ===\n",groupName)
+	if extendedClass then
+		out = string.format("%s:::::extends [[#%s|%s]]\n",out,extendedClassName,extendedClassName)
+	end
+	out = string.format("%s''%s''\n",out,GROUP_INFO[groupName] or "No Description Set")
+	out = out.."{| class=\"standard-table\" style=\"width: 100%;\"\n! Function \n! Signature \n! Description \n"
+	for funcName,userd in pairs(desc) do
+		out = string.format("%s|- [[Dota 2 Workshop Tools/Scripting/API/%s.%s | %s]] \n",out,groupName,funcName,funcName)
+		-----
+		local parameterList = {}
+		for i = 0, #userd-1 do
+			local prmType, prmName = unpack( userd[i] )
+			if prmName == nil or prmName == "" then prmName = string.format( "%s_%d", prmType, i+1 ) end
+			table.insert(parameterList,string.format("%s %s",prmType,prmName))
+		end
+		local fullFuncString = string.format("%s(%s)",funcName,table.concat(parameterList,", "))
+		----
+		out = string.format("%s| <code>%s %s </code>\n",out,userd.returnType,fullFuncString)
+		out = string.format("%s| %s \n",out,userd.desc or "No Description Set")
+	end
+	out = out.."|}\n\n"
+	return out
 end
